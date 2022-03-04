0396d6079a06283cb1a53c1405cc1f68d4816fb0
Eriko Sampaio <erikoa.93@gmail.com>
2022-03-04 19:53:16 -0300

    Criação dos testes para base de usuários
    close #24

--- libpythonpro/tests/test_spam/test_envio_para_base_de_usuarios.py
+++ libpythonpro/tests/test_spam/test_envio_para_base_de_usuarios.py
@@ -1,11 +1,61 @@
+import pytest
+
 from libpythonpro.spam.enviador_de_email import Enviador
 from libpythonpro.spam.main import EnviadorDeSpam
+from libpythonpro.spam.modelos import Usuario
 
 
-def test_envio_de_spam(sessao):
-    enviador_de_spam = EnviadorDeSpam(sessao, Enviador())
+@pytest.mark.parametrize(
+    'usuarios',
+    [
+        [
+            Usuario(nome='Eriko', email='erikoa.93@gmail.com'),
+            Usuario(nome='Nayara', email='nayara.t.95@gmail.com')
+        ],
+
+        [
+            Usuario(nome='Eriko', email='erikoa.93@gmail.com'),
+        ]
+    ]
+)
+def test_qtd_de_spam(sessao, usuarios):
+    for usuario in usuarios:
+        sessao.salvar(usuario)
+    enviador = EnviadorMock()
+    enviador_de_spam = EnviadorDeSpam(sessao, enviador)
     enviador_de_spam.enviar_emails(
-        'renzo@python.pro.br',
+        'erikoa.93@gmail.com',
         'Curso Python Pro',
         'Confira os módulos fantásticos'
     )
+    assert len(usuarios) == enviador.qtd_email_enviados
+
+
+class EnviadorMock(Enviador):
+
+    def __init__(self):
+        super().__init__()
+        self.qtd_email_enviados = 0
+        self.parametros_de_envio = None
+
+    def enviar(self, remetente, destinatario, assunto, corpo):
+        self.parametros_de_envio = (remetente, destinatario, assunto, corpo)
+        self.qtd_email_enviados += 1
+
+
+def test_parametros_de_spam(sessao):
+    usuario = Usuario(nome='Eriko', email='erikoa.93@gmail.com')
+    sessao.salvar(usuario)
+    enviador = EnviadorMock()
+    enviador_de_spam = EnviadorDeSpam(sessao, enviador)
+    enviador_de_spam.enviar_emails(
+        'nayara.t.95@gmail.com',
+        'Curso Python Pro',
+        'Confira os módulos fantásticos'
+    )
+    assert enviador.parametros_de_envio == (
+        'nayara.t.95@gmail.com',
+        'erikoa.93@gmail.com',
+        'Curso Python Pro',
+        'Confira os módulos fantásticos'
+   )
