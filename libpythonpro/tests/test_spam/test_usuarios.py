1975c6b7c2d1360f85e38dcf305fc920747e00c1
Eriko Sampaio <erikoa.93@gmail.com>
2022-03-04 19:53:16 -0300

    Criação dos testes para base de usuários
    close #24

--- libpythonpro/tests/test_spam/test_usuarios.py
+++ libpythonpro/tests/test_spam/test_usuarios.py
@@ -2,13 +2,15 @@ from libpythonpro.spam.modelos import Usuario
 
 
 def test_salvar_usuarios(sessao):
-    usuario = Usuario(nome='Eriko')
+    usuario = Usuario(nome='Eriko', email='erikoa.93@gmail.com')
     sessao.salvar(usuario)
     assert isinstance(usuario.id, int)
 
 
 def test_listar_usuarios(conexao, sessao):
-    usuarios = [Usuario(nome='Eriko'), Usuario(nome='Nayara')]
+    usuarios = [Usuario(nome='Eriko', email='erikoa.93@gmail.com'),
+                Usuario(nome='Nayara', email='nayara.t.95@gmail.com')
+                ]
     for usuario in usuarios:
         sessao.salvar(usuario)
     assert usuarios == sessao.listar()
